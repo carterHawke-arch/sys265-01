d658bcb4995933238cceb34bed6ea081646c3e43
Carter Hawke <carter.hawke@mymail.champlain.edu>
2026-02-16 21:30:53 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -8,14 +8,14 @@ username=$1
 sudo useradd -m -d /home/$username -s /bin/bash $username
 sudo mkdir -p /home/$username/.ssh
 sudo chmod 700 /home/$username/.ssh
-sudo cp ../public-keys/id_rsa.pub /home/$username/.ssh/authorized_keys
+sudo cp /home/carter/sys265-01/linux/public-keys/id_rsa.pub /home/$username/.ssh/authorized_keys
 sudo chmod 600 /home/$username/.ssh/authorized_keys
 sudo chown -R $username:$username /home/$username/.ssh
 
 sudo sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 sudo sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
-sudo systemctl restart sshd.service
+sudo systemctl restart sshd
 
 echo "Configuration complete"
 echo "Root SSH login disabled"
