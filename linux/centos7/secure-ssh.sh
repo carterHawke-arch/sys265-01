2aa8b8fdf9037ee7aa1693cdb0fdbb344c8b79a2
Carter Hawke <carter.hawke@mymail.champlain.edu>
2026-02-16 20:49:50 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,4 +3,22 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+username=$1
+
+sudo useradd -m -d /home/$username -s /bin/bash $username
+sudo mkdir -p /home/$username/.ssh
+sudo chmod 700 /home/$username/.ssh
+
+public_key_path="sys265-01/linux/public-keys/id_rsa.pub"
+
+sudo chmod 600 /home/$username/.ssh/authorized_keys
+sudo chown -R $username:$username /home/$username/.ssh
+
+sudo sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
+
+echo "Configuration complete"
+echo "Root SSH login disabled"
+echo "User $username can now login with SSH key authentication"
